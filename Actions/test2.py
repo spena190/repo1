a76628709c44f7fe61d2bfaf79e5aab022290949
spena190 <50928928+spena190@users.noreply.github.com>
2020-05-14 14:49:34 -0400

    Update test2.py

--- Actions/test2.py
+++ Actions/test2.py
@@ -1,6 +1,8 @@
 def handler(context, inputs):
     greeting = "Hello, {0}!".format(inputs["target"])
     print(greeting)
+    
+    print(" This is a test! ")
 
     outputs = {
       "greeting": greeting
